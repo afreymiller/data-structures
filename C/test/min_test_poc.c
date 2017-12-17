89b99efee8cdc7b3a50d9691a85a3321b088a91e
Adam Freymiller <afreymiller10@gmail.com>
2017-12-17 17:29:58 -0600

    adding more tests

--- C/test/min_test_poc.c
+++ C/test/min_test_poc.c
@@ -1,11 +1,19 @@
 #include "minunit.h"
+#include "../Linked_Lists/Methods.h"
 
 MU_TEST(test_check) {
 	mu_check(7 == 7);
 }
 
+MU_TEST(test_count) {
+	node* list = build_one_two_three();
+	int count = get_length(list);
+	mu_check(count == 3);
+}
+
 MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_check);
+	MU_RUN_TEST(test_count);
 }
 
 /* Arguments are optional */
