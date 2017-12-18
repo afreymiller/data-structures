48fe13c10223ececa41e52cf86345fd7b4b19355
Adam Freymiller <afreymiller10@gmail.com>
2017-12-18 06:00:52 -0600

    why is the .exe included?

--- C/test/min_test_poc.c
+++ C/test/min_test_poc.c
@@ -1,19 +1,31 @@
 #include "minunit.h"
 #include "../Linked_Lists/Methods.h"
 
-MU_TEST(test_check) {
-	mu_check(7 == 7);
-}
-
-MU_TEST(test_count) {
+MU_TEST(test_count_init) {
 	node* list = build_one_two_three();
 	int count = get_length(list);
 	mu_check(count == 3);
 }
 
+MU_TEST(test_count_add_1) {
+	node* list = build_one_two_three();
+	push(&list, 4);
+	int count = get_length(list);
+	mu_check(count == 4);
+}
+
+MU_TEST(test_count_add_2) {
+	node* list = build_one_two_three();
+	push(&list, 5);
+	push(&list, 6);
+	int count = get_length(list);
+	mu_check(count == 5);
+}
+
 MU_TEST_SUITE(test_suite) {
-	MU_RUN_TEST(test_check);
-	MU_RUN_TEST(test_count);
+	MU_RUN_TEST(test_count_init);
+	MU_RUN_TEST(test_count_add_1);
+	MU_RUN_TEST(test_count_add_2);
 }
 
 /* Arguments are optional */
